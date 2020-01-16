413e01bdeb8d10df8d5ad1be8efd896d6e96a761
Arman Alizadeh <arman@Armans-MacBook-Air.local>
2020-01-16 10:34:56 -0800

    Updates

---
+++ precache-manifest.c0cd7bb68c5173a56c5d2a1d2079783d.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51259514cb01d414d4e7396ea8e4e48d",
+    "url": "/editorial/index.html"
+  },
+  {
+    "revision": "88cdfbe4a09463ec0804",
+    "url": "/editorial/static/css/main.ee7ea19d.chunk.css"
+  },
+  {
+    "revision": "fc1293629ea106b1b8a9",
+    "url": "/editorial/static/js/2.2e4b8af0.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/editorial/static/js/2.2e4b8af0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "88cdfbe4a09463ec0804",
+    "url": "/editorial/static/js/main.80e693f6.chunk.js"
+  },
+  {
+    "revision": "a536719a443af23907c7",
+    "url": "/editorial/static/js/runtime-main.b9ce428f.js"
+  },
+  {
+    "revision": "0e7f22b7e9a7b32c5c25bc29fbb8625c",
+    "url": "/editorial/static/media/img1.0e7f22b7.jpg"
+  },
+  {
+    "revision": "0344ed1bae8973d4a701cd9e42847329",
+    "url": "/editorial/static/media/img12.0344ed1b.jpg"
+  },
+  {
+    "revision": "cf30c44563d5a090a434c419a848860d",
+    "url": "/editorial/static/media/img16.cf30c445.jpg"
+  },
+  {
+    "revision": "71254a39cbe2ab65b03c40ef8ca4f654",
+    "url": "/editorial/static/media/img2.71254a39.jpg"
+  },
+  {
+    "revision": "72456c26582ceee71ff0f9e3a152a192",
+    "url": "/editorial/static/media/img22.72456c26.jpg"
+  },
+  {
+    "revision": "861159cd20869a3e75cc76df7b09feb8",
+    "url": "/editorial/static/media/img26.861159cd.jpg"
+  },
+  {
+    "revision": "268672badcc8bb5a33946cb42ecef5fd",
+    "url": "/editorial/static/media/img3.268672ba.jpg"
+  },
+  {
+    "revision": "43f8101c342e125a51b4bfd79a2cc47f",
+    "url": "/editorial/static/media/img32.43f8101c.jpg"
+  },
+  {
+    "revision": "06e5b0bb74f186e67c1eb932a0352138",
+    "url": "/editorial/static/media/img36.06e5b0bb.jpg"
+  },
+  {
+    "revision": "6e7bab33414804b7dbf34e08874afc7b",
+    "url": "/editorial/static/media/sprite.6e7bab33.svg"
+  }
+]);
